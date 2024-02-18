3c8ed6bf3aed3c0b6bc8f3d9b570d1c702f50293
kontchakov <roman@dcs.bbk.ac.uk>
2024-02-18 09:22:08 +0000

    nested classes example

--- week2/src/classes/NestedClassDemo.java
+++ week2/src/classes/NestedClassDemo.java
@@ -1,22 +1,29 @@
 package classes;
 
+
+import java.util.LinkedList;
+
 class OuterClass {
     private int variable = 1;
     class InnerClass {
         private int variable = 2; // shadows variable from OuterClass
 
+        // An instance of InnerClass can exist only within an instance of OuterClass
+        // and has direct access to the methods and fields of its enclosing instance.
         int method1(int variable) {
             return this.variable; // refers to variable from InnerClass
         }
         int method2() {
+            OuterClass outer = OuterClass.this;  // refers to the enclosing instance
+            // return outer.variable; // refers to variable from OuterClass
             return OuterClass.this.variable; // refers to variable from OuterClass
         }
     }
 
     static class StaticNestedClass {
         int method() {
-            //return var; // not accessible
-            //return OuterClass.this.var; // Outer.this not available
+            //return variable; // not accessible
+            //return OuterClass.this.variable; // Outer.this not available
             return 0;
         }
     }
@@ -35,6 +42,7 @@ public class NestedClassDemo {
      */
     public static void main(String... args) {
         OuterClass outer = new OuterClass();
+        OuterClass.InnerClass inner0 = outer.method();
         OuterClass.InnerClass inner = outer.new InnerClass();
         System.out.println(inner.method1(42));
         System.out.println(inner.method2());
